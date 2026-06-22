40d2f6407640e0400678cff3c3b0eeb39bfe411e
Shivesh <drshivesh@gmail.com>
2026-06-22 13:27:24 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CardiogenicShockHemodynamics from "./CardiogenicShockHemodynamics.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CardiogenicShockHemodynamics />
+  </React.StrictMode>
+);
